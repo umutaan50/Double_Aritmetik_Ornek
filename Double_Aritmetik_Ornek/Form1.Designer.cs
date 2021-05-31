5f3dbffd8600a7a3741b86ca4f9658de7188de81
umutaan50 <umutaan50@gmail.com>
2021-05-31 17:48:04 +0300

    Add project files.

---
+++ Double_Aritmetik_Ornek/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Double_Aritmetik_Ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbltoplam = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblbölüm = new System.Windows.Forms.Label();
+            this.lblçarpım = new System.Windows.Forms.Label();
+            this.lblçıkarım = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(284, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(61, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Sayı";
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(61, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. Sayı";
+            this.label2.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(198, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 36);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(198, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 36);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(61, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Toplam:";
+            this.label3.UseWaitCursor = true;
+            // 
+            // lbltoplam
+            // 
+            this.lbltoplam.AutoSize = true;
+            this.lbltoplam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lbltoplam.Location = new System.Drawing.Point(205, 211);
+            this.lbltoplam.Name = "lbltoplam";
+            this.lbltoplam.Size = new System.Drawing.Size(27, 29);
+            this.lbltoplam.TabIndex = 6;
+            this.lbltoplam.Text = "?";
+            this.lbltoplam.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(61, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Çarpım:";
+            this.label4.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.Location = new System.Drawing.Point(61, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Çıkarım:";
+            this.label5.UseWaitCursor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label6.Location = new System.Drawing.Point(61, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 29);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Bölüm:";
+            this.label6.UseWaitCursor = true;
+            // 
+            // lblbölüm
+            // 
+            this.lblbölüm.AutoSize = true;
+            this.lblbölüm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblbölüm.Location = new System.Drawing.Point(205, 381);
+            this.lblbölüm.Name = "lblbölüm";
+            this.lblbölüm.Size = new System.Drawing.Size(27, 29);
+            this.lblbölüm.TabIndex = 10;
+            this.lblbölüm.Text = "?";
+            this.lblbölüm.UseWaitCursor = true;
+            // 
+            // lblçarpım
+            // 
+            this.lblçarpım.AutoSize = true;
+            this.lblçarpım.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblçarpım.Location = new System.Drawing.Point(205, 326);
+            this.lblçarpım.Name = "lblçarpım";
+            this.lblçarpım.Size = new System.Drawing.Size(27, 29);
+            this.lblçarpım.TabIndex = 10;
+            this.lblçarpım.Text = "?";
+            this.lblçarpım.UseWaitCursor = true;
+            // 
+            // lblçıkarım
+            // 
+            this.lblçıkarım.AutoSize = true;
+            this.lblçıkarım.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblçıkarım.Location = new System.Drawing.Point(205, 269);
+            this.lblçıkarım.Name = "lblçıkarım";
+            this.lblçıkarım.Size = new System.Drawing.Size(27, 29);
+            this.lblçıkarım.TabIndex = 11;
+            this.lblçıkarım.Text = "?";
+            this.lblçıkarım.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(821, 484);
+            this.Controls.Add(this.lblçıkarım);
+            this.Controls.Add(this.lblçarpım);
+            this.Controls.Add(this.lblbölüm);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbltoplam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Doble Aritmetik";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltoplam;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblbölüm;
+        private System.Windows.Forms.Label lblçarpım;
+        private System.Windows.Forms.Label lblçıkarım;
+    }
+}
+
---
+++ Double_Aritmetik_Ornek/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Double_Aritmetik_Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayı1, sayı2, toplam, çarpım, bölüm, çıkarım;
+            sayı1 = Convert.ToDouble(textBox1.Text);
+            sayı2 = Convert.ToDouble(textBox2.Text);
+
+            toplam = sayı1 + sayı2;
+            lbltoplam.Text = toplam.ToString();
+            çarpım = sayı1 * sayı2; bölüm = sayı1 / sayı2; çıkarım = sayı1 - sayı2;
+            lblbölüm.Text = bölüm.ToString(); lblçarpım.Text = çarpım.ToString(); lblçıkarım.Text = çıkarım.ToString();
+            
+
+
+
+        }
+    }
+}
